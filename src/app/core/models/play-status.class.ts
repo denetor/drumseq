c665bd507f8ab085c4efe1b4558997e60680683f
Nicola Da Rold <ndarold@pataviumenergia.it>
2024-11-20 12:04:29 +0100

    BPM play status in player component
    Add `PlayStatus` class to track the row, measure, beat, and quarter. Updated `player.component.ts` to use and advance this status during playback, and display it in the HTML template.

---
+++ src/app/core/models/play-status.class.ts
@@ -0,0 +1,13 @@
+export class PlayStatus {
+  row: number;
+  measure: number;
+  beat: number;
+  quarter: number;
+
+  constructor() {
+    this.row = 0;
+    this.measure = 0;
+    this.beat = 0;
+    this.quarter = 0;
+  }
+}
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Project} from '../core/models/project.class';
 import {TabViewerComponent} from '../components/tab-viewer/tab-viewer.component';
 import {interval, Observable, Subscription, take} from 'rxjs';
+import {PlayStatus} from '../core/models/play-status.class';
 
 @Component({
   selector: 'app-player',
@@ -16,11 +17,13 @@ export class PlayerComponent implements OnInit, OnDestroy {
   project: Project;
   beatQuarter$: Observable<number>;
   beatQuarterSubscription: Subscription;
+  playStatus: PlayStatus;
 
   constructor() {
     this.project = new Project();
     this.beatQuarter$ = new Observable();
     this.beatQuarterSubscription = new Subscription();
+    this.playStatus = new PlayStatus();
   }
 
   ngOnInit() {
@@ -32,15 +35,40 @@ export class PlayerComponent implements OnInit, OnDestroy {
   }
 
   play() {
+    this.playStatus = new PlayStatus();
     this.beatQuarter$ = interval(60000 / this.project.configuration.bpm / 4);
     this.beatQuarterSubscription = this.beatQuarter$.subscribe(
-      (beat) => {
-        console.log(beat);
+      (tick) => {
+        this.playStatus = this.advanceTick(this.project, this.playStatus);
       }
     );
   }
 
   stop() {
     this.beatQuarterSubscription.unsubscribe();
+  }
+
+
+  advanceTick(project: Project, status: PlayStatus): PlayStatus {
+    console.log('advanceTick()');
+    status.quarter++;
+    if (status.quarter >= 4) {
+      status.quarter = 0;
+      status.beat++;
+    }
+    if (status.beat >= this.project.configuration.beatsPerMeasure) {
+      status.beat = 0;
+      status.measure++;
+    }
+    if (status.measure >= this.project.configuration.measuresPerBar) {
+      status.measure = 0;
+      status.row++;
+    }
+    if (status.row >= project.rows.length) {
+      status.row = 0;
+      this.stop();
+    }
+    console.log(status);
+    return status;
   }
 }
