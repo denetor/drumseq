bff91f942cbeb724add04f344ecfdf4a0444b532
Nicola Da Rold <nicola.darold@openview.it>
2024-11-20 23:04:15 +0100

    Adjust initial quarter value and update .gitignore
    Changed the initial value of `quarter` from 0 to -1 in `play-status.class.ts` for better state management.

--- src/app/core/models/play-status.class.ts
+++ src/app/core/models/play-status.class.ts
@@ -9,7 +9,7 @@ export class PlayStatus {
     this.row = 0;
     this.measure = 0;
     this.beat = 0;
-    this.quarter = 0;
+    this.quarter = -1;
     this.metronome = false;
   }
 
@@ -17,6 +17,6 @@ export class PlayStatus {
     this.row = 0;
     this.measure = 0;
     this.beat = 0;
-    this.quarter = 0;
+    this.quarter = -1;
   }
 }
