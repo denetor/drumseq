2defeb4494498b4617e8209b6e1ff87804afa8e1
Nicola Da Rold <ndarold@pataviumenergia.it>
2024-11-20 11:39:45 +0100

    Add play/stop buttons and manage beat intervals
    Added play and stop buttons to the player component template to control playback. Introduced functions to manage the beat intervals using RxJS observables and handle subscription cleanup on component destruction.

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Project} from '../core/models/project.class';
 import {TabViewerComponent} from '../components/tab-viewer/tab-viewer.component';
+import {interval, Observable, Subscription, take} from 'rxjs';
 
 @Component({
   selector: 'app-player',
@@ -11,14 +12,35 @@ import {TabViewerComponent} from '../components/tab-viewer/tab-viewer.component'
   templateUrl: './player.component.html',
   styleUrl: './player.component.sass'
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements OnInit, OnDestroy {
   project: Project;
+  beatQuarter$: Observable<number>;
+  beatQuarterSubscription: Subscription;
 
   constructor() {
     this.project = new Project();
+    this.beatQuarter$ = new Observable();
+    this.beatQuarterSubscription = new Subscription();
   }
 
   ngOnInit() {
     this.project = new Project();
+  }
+
+  ngOnDestroy() {
+    this.beatQuarterSubscription.unsubscribe();
+  }
+
+  play() {
+    this.beatQuarter$ = interval(60000 / this.project.configuration.bpm / 4);
+    this.beatQuarterSubscription = this.beatQuarter$.subscribe(
+      (beat) => {
+        console.log(beat);
+      }
+    );
+  }
+
+  stop() {
+    this.beatQuarterSubscription.unsubscribe();
   }
 }
